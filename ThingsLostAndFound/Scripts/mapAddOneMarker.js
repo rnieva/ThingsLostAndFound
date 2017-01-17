70fed636c17e73a4302c49c3d464e15a92afbba1
rnieva <roberto.nieva@gmail.com>
2017-01-17 11:21:43 +0000

    Added map in Objects Details. Added js script that Add a marker.

---
+++ ThingsLostAndFound/Scripts/mapAddOneMarker.js
@@ -0,0 +1,12 @@
+﻿//Add a marker to the map. It show on Object Details
+function addMarker(mapLocation,title) {
+    var latlngStr = (mapLocation).split(',', 2);
+    var latlng = { lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1]) };
+    var marker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+        title: title
+    });
+    map.setCenter(latlng);
+    map.setZoom(8);
+}
---
+++ ThingsLostAndFound/Scripts/mapInit.js
@@ -0,0 +1,35 @@
+﻿// initiate the map. 
+initMap()
+var map;
+
+function initMap() {
+    var centerPosition = new google.maps.LatLng(55.942071, -3.200165);
+    var options = {
+        zoom: 11,
+        center: centerPosition,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map($('#map_canvas')[0], options);
+    if (navigator.geolocation) //center the map in user location
+    {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
+}
