79e12131c1e341a9d339aa3980e57cb504c0ad4d
rnieva <roberto.nieva@gmail.com>
2017-02-08 10:47:32 +0000

    Added new script file called validateDeleteObject.js for using in FO and LO Delete views

---
+++ ThingsLostAndFound/Scripts/validateDeleteObject.js
@@ -0,0 +1,26 @@
+﻿// validate if the user has checked some check box when he wants to delete an object
+
+function validate() {
+    if ((checkObjectSolved.checked == false) && (checkUserCreateObject.checked == false)) {
+        alert('Please check at least one of the option.');
+        return false;
+    }
+    else {
+        if ((checkObjectSolved.checked == true) && (checkUserCreateObject.checked == false)) {
+            if (document.forms["form"]["nameContact"].value == "") {
+                alert('Please write the contact name');
+                return false;
+            }
+            return true;
+        }
+        else {
+            if ((checkObjectSolved.checked == false) && (checkUserCreateObject.checked == true)) {
+                return true; //checkUserCreateObject I wanna forget it
+            } else {
+                alert('Please check only one of the option.');
+                return false;
+            }
+        }
+
+    }
+}
