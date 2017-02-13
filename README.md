71f588beb90e2cde979cdc35ed1c7da4d396a687
rnieva <roberto.nieva@gmail.com>
2017-02-13 12:18:11 +0000

    Some desigh changes. Added new info to readme file.

--- README.md
+++ README.md
@@ -6,3 +6,26 @@ A self-taught project to learn more about Model–View–Controller (MVC),C#, Maps a
 
 ---
 Add file PrivateSettings.config in c:\ path with private data, example in this folder.
+
+---
+Things Lost And Found Manual
+
+Menu:
+ThingsLostAndFound --> Link to Home.
+
+
+
+---
+How work User Login?
+Create a new User.
+1- The user into data to create a new user.
+2- If the NameUser is not using, the process go on.
+3- It create a new Salt using "getSalt" method in "Crypto.cs".
+4- It create a new crypto password using the "Hash" method in "Crypto.cs" with tha Salt and password.
+5- It strore data in DB. 
+
+User Login:
+1- The user into UserName and Password in Login View.
+2- The controller "LoginController.cs" get these data, retrive the Salt for that UserName and use the method Hash in "Crypto.cs" to check the password.
+3- If it´s right it go to Home Page.
+---
--- ThingsLostAndFound/Controllers/ShowObjectUsersController.cs
+++ ThingsLostAndFound/Controllers/ShowObjectUsersController.cs
@@ -11,15 +11,15 @@ namespace ThingsLostAndFound.Controllers
     public class ShowObjectUsersController : Controller  
     {
         private TLAFEntities db = new TLAFEntities();
-        public ActionResult ShowObjects(int id) // user id registered , show the objects created by the user
+        public ActionResult ShowObjects(int id, int? page) // user id registered , show the objects created by the user
         {
             HttpCookie authCookie = Request.Cookies[FormsAuthentication.FormsCookieName];
             FormsAuthenticationTicket ticket = FormsAuthentication.Decrypt(authCookie.Value);
             string infoUserIdRolNewM = ticket.UserData.ToString();
             int userId = Int32.Parse(infoUserIdRolNewM.Substring(0, infoUserIdRolNewM.IndexOf("|")));
             int roll = Int32.Parse(infoUserIdRolNewM.Substring((infoUserIdRolNewM.IndexOf("|")) + 1, (infoUserIdRolNewM.IndexOf("||") - infoUserIdRolNewM.IndexOf("|") - 1)));
-            List<object> objectsList = new List<object>();
             int objectNumber = 0;
+            List<object> objectsList = new List<object>();
             if ((id == userId) || (roll == 1))     // This way, only the user with id can see
             {
                 var objFoundList = db.FoundObjects.Where(a => a.UserIdreported == id).ToList();
